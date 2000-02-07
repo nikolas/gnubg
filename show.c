5fc222271597de73b72e0c95933147713d7451f4
Gary Wong <gtw@gnu.org>
2000-02-07 18:57:35 +0000

    Use GetInput() when waiting in ShowPaged().

--- show.c
+++ show.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: show.c,v 1.10 2000/01/14 20:44:37 gtw Exp $
+ * $Id: show.c,v 1.11 2000/01/15 17:38:21 gtw Exp $
  */
 
 #include "config.h"
@@ -79,11 +79,7 @@ static void ShowPaged( char **ppch ) {
 	while( *ppch ) {
 	    puts( *ppch++ );
 	    if( ++i >= nRows - 1 ) {
-		fputs( "-- Press <return> to continue --", stdout );
-		
-		/* FIXME use better input handling */
-		while( ( ch = getchar() ) != '\n' && ch != EOF )
-		    ;
+		GetInput( "-- Press <return> to continue --" );
 		
 		if( fInterrupt )
 		    return;
