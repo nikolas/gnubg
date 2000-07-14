0982dd1537ea82c7ea8c44d6e0e056a6b8185180
Gary Wong <gtw@gnu.org>
2000-07-14 18:06:51 +0000

    Added MD5 dice generator.

--- backgammon.h
+++ backgammon.h
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: backgammon.h,v 1.31.2.3 2000/07/13 15:56:32 gtw Exp $
+ * $Id: backgammon.h,v 1.32 2000/07/13 16:25:26 gtw Exp $
  */
 
 #ifndef _BACKGAMMON_H_
@@ -196,6 +196,7 @@ extern void CommandAccept( char * ),
     CommandSetRNGBsd( char * ),
     CommandSetRNGIsaac( char * ),
     CommandSetRNGManual( char * ),
+    CommandSetRNGMD5( char * ),
     CommandSetRNGMersenne( char * ),
     CommandSetRNGUser( char * ),
     CommandSetRolloutEvaluation( char * ),
