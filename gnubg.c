685795d40fc37c204b86c09bee8b7fc990200e5f
Gary Wong <gtw@gnu.org>
2001-04-18 17:12:11 +0000

    Replace the summary struct with the more detailed Statcontext.

--- gnubg.c
+++ gnubg.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: gnubg.c,v 1.128 2001/04/17 15:49:55 oysteijo Exp $
+ * $Id: gnubg.c,v 1.129 2001/04/18 14:16:47 gtw Exp $
  */
 
 #include "config.h"
@@ -3489,7 +3489,7 @@ static void real_main( void *closure, int argc, char *argv[] ) {
 #endif
     
     ListCreate( &lMatch );
-    ClearSummary( &sMatch );
+    IniStatcontext( &scMatch );
     
 #if USE_GTK
     if( fTTY )
