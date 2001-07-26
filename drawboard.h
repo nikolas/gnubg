55de5400bfc5e759949dffb7ba8c265c35462771
Gary Wong <gtw@gnu.org>
2001-07-26 14:45:31 +0000

    Add option for player 1 to move clockwise (contributed by Tristan Colgate).

--- drawboard.h
+++ drawboard.h
@@ -16,11 +16,13 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: drawboard.h,v 1.6 2000/12/16 23:57:21 oysteijo Exp $
+ * $Id: drawboard.h,v 1.7 2001/02/13 18:22:43 gtw Exp $
  */
 
 #ifndef _DRAWBOARD_H_
 #define _DRAWBOARD_H_
+
+extern int fClockwise; /* Player 1 moves clockwise */
 
 extern char *DrawBoard( char *pch, int anBoard[ 2 ][ 25 ], int fRoll,
                         char *asz[] );
