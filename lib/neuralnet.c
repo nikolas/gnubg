0fe90bed2ce8620f74cb8e9a7348b9e803b6bf94
Øystein Schønning-Johansen <oystein@gnubg.org>
2002-04-27 06:17:07 +0000

    Save files in UNIX format

--- lib/neuralnet.c
+++ lib/neuralnet.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id$
+ * $Id: neuralnet.c,v 1.16 2002/04/26 17:39:47 oysteijo Exp $
  */
 
 #include "config.h"
