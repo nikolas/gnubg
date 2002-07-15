c06f3e0925cac7c0a5c4e92ceea956885ff81032
Joern Thyssen <jth@gnubg.org>
2002-07-15 21:09:59 +0000

    add RCS keyword

--- import.c
+++ import.c
@@ -16,6 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
+ * $Id: eval.c,v 1.161 2002/07/14 16:36:22 thyssen Exp $
  */
 
 #include "config.h"
