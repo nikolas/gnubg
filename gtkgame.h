c913f749bb9b5ef0ee4291a6b7fb95db04286ceb
Joern Thyssen <jth@gnubg.org>
2002-07-17 11:51:41 +0000

    move all cube, take, and chequer play analysis widgets into other files (trimming gtkgame.c a bit).  CVS: ----------------------------------------------------------------------

--- gtkgame.h
+++ gtkgame.h
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: gtkgame.h,v 1.41 2002/05/22 18:56:47 thyssen Exp $
+ * $Id: gtkgame.h,v 1.42 2002/07/16 17:39:29 gtw Exp $
  */
 
 #ifndef _GTKGAME_H_
@@ -122,6 +122,10 @@ extern void GTKSetCube( gpointer *p, guint n, GtkWidget *pw );
 extern void GTKHelp( char *sz );
 extern void 
 GTKShowPath( void );
+
+extern void SetEvalChequer( gpointer *p, guint n, GtkWidget *pw );
+extern void SetEvalCube( gpointer *p, guint n, GtkWidget *pw );
+extern void SetRollouts( gpointer *p, guint n, GtkWidget *pw );
 
 extern int fTTY;
 
