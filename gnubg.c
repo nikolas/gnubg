16268c6112979e0a727176b644bcef0503b2aa57
Joern Thyssen <jth@gnubg.org>
2002-10-09 18:58:15 +0000

    update /Game/Turn/ menu after swapping player names.

--- gnubg.c
+++ gnubg.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: gnubg.c,v 1.310 2002/10/07 21:01:45 thyssen Exp $
+ * $Id: gnubg.c,v 1.311 2002/10/09 18:44:36 thyssen Exp $
  */
 
 #include "config.h"
@@ -6113,6 +6113,7 @@ extern void CommandSwapPlayers ( char *sz ) {
 
 #if USE_GTK
   if ( fX ) {
+    GTKSet ( ap );
     GTKRegenerateGames();
     GTKUpdateAnnotations();
   }
