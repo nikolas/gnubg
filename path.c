dbc11285d25bdfd2730607f98b7d7ef7e27d9a87
Joern Thyssen <jth@gnubg.org>
2002-12-12 17:11:50 +0000

    can't use outputerr here,

--- path.c
+++ path.c
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: path.c,v 1.1 2002/12/03 22:47:23 jsegrave Exp $
+ * $Id: path.c,v 1.2 2002/12/11 20:21:34 thyssen Exp $
  */
 
 #include "config.h"
@@ -171,7 +171,7 @@ BackupFile ( const char *sz ) {
   /* windows can not rename to an existing file */
   if ( unlink ( szNew ) && errno != ENOENT ) {
     /* do not complain if file is not found */
-    outputerr ( szNew );
+    perror ( szNew );
     free ( szNew );
     return -1;
   }
