d54651ff47d91f4e5b5605b1e79ae8be081ae94d
Gary Wong <gtw@gnu.org>
2003-01-17 17:57:17 +0000

    Allow clicking on dice area (to set dice) and label chequers (to set player on roll).

--- gtkgame.h
+++ gtkgame.h
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: gtkgame.h,v 1.57 2002/12/27 15:07:17 thyssen Exp $
+ * $Id$
  */
 
 #ifndef _GTKGAME_H_
@@ -46,6 +46,7 @@ typedef enum _dialogtype {
 } dialogtype;
 
 extern GtkWidget *pwMain, *pwMenuBar;
+extern GtkTooltips *ptt;
 
 extern int fEndDelay;
 
