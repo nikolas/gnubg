be6b8d9b89c212f6a94cd64754709646f3b32914
Joern Thyssen <jth@gnubg.org>
2003-06-06 09:27:17 +0000

    modify formulaes for snowie error rate according to Chris Yep's email.

--- html.c
+++ html.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: html.c,v 1.103 2003/04/08 20:54:34 thyssen Exp $
+ * $Id: html.c,v 1.104 2003/05/03 08:11:24 thyssen Exp $
  */
 
 #include "config.h"
@@ -1850,7 +1850,7 @@ HTMLEpilogue ( FILE *pf, const matchstate *pms, char *aszLinks[ 4 ],
   int fFirst;
   int i;
 
-  const char szVersion[] = "$Revision: 1.103 $";
+  const char szVersion[] = "$Revision: 1.104 $";
   int iMajor, iMinor;
 
   iMajor = atoi ( strchr ( szVersion, ' ' ) );
@@ -1931,7 +1931,7 @@ HTMLEpilogueComment ( FILE *pf ) {
 
   time_t t;
 
-  const char szVersion[] = "$Revision: 1.103 $";
+  const char szVersion[] = "$Revision: 1.104 $";
   int iMajor, iMinor;
   char *pc;
 
@@ -3302,7 +3302,7 @@ static void HTMLDumpStatcontext ( FILE *pf, const statcontext *psc,
         char asz[ 2 ][ 16 ];
 
         for ( i = 0; i < 2; ++i )
-          if ( ( j = psc->anTotalMoves[ i ] + psc->anTotalCube[ i ] ) )
+          if ( ( j = psc->anTotalMoves[ 0 ] + psc->anTotalMoves[ 1 ] ) )
             sprintf( asz[ i ], "%.1f", 1000.0 * 
                      -aaaar[ COMBINED ][ TOTAL ][ i ][ NORMALISED ] / j );
           else
