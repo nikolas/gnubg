29fd9262f10cc3adac8ca10d4f2fdc579d5c8dd4
Jon Kinsey <jonkinsey@gmail.com>
2003-07-07 11:24:42 +0000

    3d preview and other misc changes

--- board3d/mylist.h
+++ board3d/mylist.h
@@ -1,3 +1,25 @@
+/*
+* mylist.h
+* by Jon Kinsey, 2003
+*
+* Simple list
+*
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of version 2 of the GNU General Public License as
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*
+* $Id: drawboard3d.c,v 1.1.2.10 2003/06/25 09:35:15 Superfly_Jon Exp $
+*/
 
 typedef struct _myList
 {
