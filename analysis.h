5cf84d2a5b3c5b041bf68fe956af38c9c934f01f
Joern Thyssen <jth@gnubg.org>
2003-07-17 10:33:02 +0000

    Some changes to the match statistics

--- analysis.h
+++ analysis.h
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: analysis.h,v 1.17 2003/04/05 18:02:04 thyssen Exp $
+ * $Id: analysis.h,v 1.18 2003/06/25 19:16:10 thyssen Exp $
  */
 
 #ifndef _ANALYSIS_H_
@@ -75,6 +75,9 @@ typedef struct _statcontext {
   /* luck adjusted result */
 
   float arActualResult[ 2 ];
+  float arVarianceActual[ 2 ];
+  float arVarianceLuckAdj[ 2 ];
+  int nGames;
 
 } statcontext;
 
