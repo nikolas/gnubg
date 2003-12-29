8c14b960a0893cc933fa98bccb3e4615475c1cb1
Joern Thyssen <jth@gnubg.org>
2003-12-29 20:22:33 +0000

    fix font bug introduced earlier

--- gtkbearoff.h
+++ gtkbearoff.h
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: gtkbearoff.h,v 1.1.2.1 2003/08/05 07:54:10 Superfly_Jon Exp $
+ * $Id: gtkbearoff.h,v 1.2 2003/08/13 11:52:27 Superfly_Jon Exp $
  */
 
 #ifndef _GTKBEAROFF_H_
@@ -24,5 +24,8 @@
 
 extern void
 GTKShowBearoff( const matchstate *pms );
+
+extern void
+GTKShowEPC( int anBoard[ 2 ][ 25 ] );
 
 #endif /* _GTKBEAROFF_H_ */
