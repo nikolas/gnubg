fc2f9b19ef6933923b8455a1d76765a8f29a962c
Joern Thyssen <jth@gnubg.org>
2003-12-29 19:21:39 +0000

    compile error: move "moverecord *pmr" outside USE_GTK.  (reported by Nardy)

--- gnubg.c
+++ gnubg.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: gnubg.c,v 1.513 2003/12/02 14:15:12 Superfly_Jon Exp $
+ * $Id$
  */
 
 #include "config.h"
@@ -3966,6 +3966,7 @@ HintChequer( char *sz ) {
   char szBuf[ 1024 ];
   int n = ParseNumber ( &sz );
   int anMove[ 8 ];
+#endif
   moverecord *pmr;
   unsigned char auch[ 10 ];
   int fHasMoved;
@@ -3994,7 +3995,7 @@ HintChequer( char *sz ) {
     MoveKey ( ms.anBoard, anMove, auch );
     fHasMoved = TRUE;
   }
-#endif /* ! USE_GTK */
+#endif /* USE_GTK */
 
   if ( !fHasMoved && plLastMove && ( pmr = plLastMove->plNext->p ) && 
        pmr->mt == MOVE_NORMAL ) {
