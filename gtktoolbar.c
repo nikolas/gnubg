f6d630dd8ed0ed3fa60664e805b49a11829cdeaf
Joern Thyssen <jth@gnubg.org>
2004-01-01 11:50:15 +0000

    disable edit button if there is no match in progress (reported by Misja Alma)

--- gtktoolbar.c
+++ gtktoolbar.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: gtktoolbar.c,v 1.7 2003/08/29 18:38:12 jsegrave Exp $
+ * $Id: gtktoolbar.c,v 1.8 2003/09/29 07:35:44 Superfly_Jon Exp $
  */
 
 #if HAVE_CONFIG_H
@@ -324,6 +324,7 @@ ToolbarUpdate ( GtkWidget *pwToolbar,
   gtk_widget_set_sensitive ( ptw->pwExport,  plGame != NULL);
   gtk_widget_set_sensitive ( ptw->pwResign, fPlaying  && !fEdit);
   gtk_widget_set_sensitive ( ptw->pwHint, fPlaying  && !fEdit);
+  gtk_widget_set_sensitive ( ptw->pwEdit, plGame != NULL );
 
   return c;
 
