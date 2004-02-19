5683c03ac89db2f52767e3edd5ba63eb3b5df952
Jon Kinsey Jon Kinsey <jonkinsey@gmail.com>
2004-02-19 09:42:10 +0000

    Added new directory to cvs for python scripts

--- scripts/gnubg.py
+++ scripts/gnubg.py
@@ -23,7 +23,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-# $Id: gnubg.py,v 1.2 2003/08/13 11:52:27 Superfly_Jon Exp $
+# $Id: gnubg.py,v 1.3 2003/08/18 22:21:30 joseph Exp $
 #
 
 
