7a0ec2a4b83cbe86c7241eeed4760373c10fc6a1
Joern Thyssen <jth@gnubg.org>
2004-04-11 20:22:28 +0000

    define a NO_GUI variant of function

--- set.c
+++ set.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: set.c,v 1.222 2004/03/31 09:51:56 Superfly_Jon Exp $
+ * $Id: set.c,v 1.223 2004/04/08 14:22:05 thyssen Exp $
  */
 
 #include "config.h"
@@ -1265,6 +1265,11 @@ extern void CommandSetGUIShowIDs( char *sz ) {
     NoGUI();
 }
 
+extern void CommandSetGUIShowEPCs( char *sz ) {
+
+    NoGUI();
+}
+ 
 extern void CommandSetGUIShowPips( char *sz ) {
 
     NoGUI();
