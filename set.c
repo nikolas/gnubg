dc3ee2b43051c127bcdeaf24d3a596239c1ab496
Jon Kinsey <jonkinsey@gmail.com>
2004-05-19 08:44:36 +0000

    Vary board tesselation based on accuracy setting

--- set.c
+++ set.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: set.c,v 1.235 2004/05/08 09:11:07 Superfly_Jon Exp $
+ * $Id: set.c,v 1.236 2004/05/10 19:37:44 thyssen Exp $
  */
 
 #include "config.h"
@@ -82,6 +82,11 @@
 
 #if USE_TIMECONTROL
 #include "timecontrol.h"
+#endif
+
+#if WIN32
+/* Needed for thread priority defines */
+#include <windows.h>
 #endif
 
 static int iPlayerSet, iPlayerLateSet;
