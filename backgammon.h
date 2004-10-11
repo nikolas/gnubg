5b705dd37a2808b8d6d2a963bef317dfc7b7c61e
Jon Nall <nall@themountaingoats.net>
2004-10-11 00:05:36 +0000

    - added support for annotating moves with comments via the command line

--- backgammon.h
+++ backgammon.h
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: backgammon.h,v 1.293 2004/10/05 16:13:00 Superfly_Jon Exp $
+ * $Id: backgammon.h,v 1.294 2004/10/07 11:24:06 Superfly_Jon Exp $
  */
 
 #ifndef _BACKGAMMON_H_
@@ -777,6 +777,7 @@ extern void CommandAccept( char * ),
     CommandAnalyseSession( char * ),
     CommandAnnotateAccept ( char * ),
     CommandAnnotateBad( char * ),
+    CommandAnnotateAddComment( char * ),
     CommandAnnotateClearComment( char * ),
     CommandAnnotateClearLuck( char * ),
     CommandAnnotateClearSkill( char * ),
