198a6a653b0a713e0f3035841b1f808018fdfeeb
Jim Segrave <jes@j-e-s.net>
2004-10-17 22:20:46 +0000

    add new sgf versions for pruning, compiler warning fixes, bad bmp file

--- gtkpanels.c
+++ gtkpanels.c
@@ -18,10 +18,11 @@
 * along with this program; if not, write to the Free Software
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 *
-* $Id: gtkpanels.c,v 1.3 2004/09/13 14:30:18 Superfly_Jon Exp $
+* $Id: gtkpanels.c,v 1.4 2004/09/16 07:50:17 Superfly_Jon Exp $
 */
 
 #include "config.h"
+#include <stdlib.h>
 #define GTK_ENABLE_BROKEN /* for GtkText */
 #include "backgammon.h"
 #include "gtkboard.h"
