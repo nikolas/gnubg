f54ec32f9678b4130f6501bdab9dd5eb6ee0b9c1
Christian Anthon <anthon@kiku.dk>
2006-06-02 21:46:14 +0000

    compiler warnings

--- gtkmovelist.c
+++ gtkmovelist.c
@@ -17,12 +17,13 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: gtkmovelist.c,v 1.4 2005/10/19 06:36:40 Superfly_Jon Exp $
+ * $Id: gtkmovelist.c,v 1.5 2006/02/24 17:41:47 Superfly_Jon Exp $
  */
 
 #include <config.h>
 
 #include <gtk/gtk.h>
+#include <string.h>
 #include "eval.h"
 #include "gtkchequer.h"
 #include "i18n.h"
