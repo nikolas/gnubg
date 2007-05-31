9dd63fab50bc3278d978f74b4f7956c32b184ee9
Christian Anthon <anthon@kiku.dk>
2007-05-31 22:30:24 +0000

    fix some problems with setting the web browser

--- openurl.h
+++ openurl.h
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: openurl.h,v 1.1 2003/05/21 19:23:13 thyssen Exp $
+ * $Id: openurl.h,v 1.2 2007/03/20 18:37:54 c_anthon Exp $
  */
 
 #ifndef _OPENURL_H_
@@ -25,6 +25,6 @@
 
 extern void OpenURL( const char *szURL );
 extern char *set_web_browser (const char *sz);
-extern char *get_web_browser (void);
+extern const gchar *get_web_browser (void);
 
 #endif /* _OPENURL_H_ */
