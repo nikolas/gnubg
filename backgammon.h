194c75b1e19eb88b4bb2144572f277f8d929b634
Christian Anthon <anthon@kiku.dk>
2007-11-16 22:34:18 +0000

    cleanup gtkgame.h

--- backgammon.h
+++ backgammon.h
@@ -16,16 +16,14 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: backgammon.h,v 1.350 2007/10/18 14:27:52 Superfly_Jon Exp $
+ * $Id: backgammon.h,v 1.351 2007/11/16 21:13:17 c_anthon Exp $
  */
 
 #ifndef _BACKGAMMON_H_
 #define _BACKGAMMON_H_
 
-#include "list.h"
 #include "analysis.h"
 #include "eval.h"
-#include "common.h"
 
 #define MAX_CUBE ( 1 << 12 )
 #define MAX_NAME_LEN 32
