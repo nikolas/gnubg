fb0b7ad15857a832aba3970929efb000462a3b9d
Jon Kinsey <jonkinsey@gmail.com>
2008-04-15 21:11:54 +0000

    Minor changes

--- eval.h
+++ eval.h
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: eval.h,v 1.152 2008/02/20 22:57:48 c_anthon Exp $
+ * $Id: eval.h,v 1.153 2008/03/12 22:56:33 Superfly_Jon Exp $
  */
 
 #ifndef _EVAL_H_
@@ -288,7 +288,6 @@ typedef struct {
 extern int fInterrupt;
 extern cubeinfo ciCubeless;
 extern const char *aszEvalType[ (int)EVAL_ROLLOUT + 1 ];
-extern int fEgyptian;
 
 extern bearoffcontext *pbc1;
 extern bearoffcontext *pbc2;
