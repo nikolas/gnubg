5cccb6a269548d17a7497b97571e1887b74cfedb
Christian Anthon <anthon@kiku.dk>
2008-06-06 15:44:53 +0000

    install documentation in DOCDIR

--- util.h
+++ util.h
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: util.h,v 1.7 2008/01/17 22:28:05 Superfly_Jon Exp $
+ * $Id: util.h,v 1.8 2008/02/15 12:04:35 c_anthon Exp $
  */
 
 #ifndef _UTIL_H_
@@ -25,6 +25,7 @@
 #include "stdio.h"
 
 #ifdef WIN32
+#define DOCDIR getDocDir()
 #define BuildFilename(file) g_build_filename(getInstallDir(), file, NULL)
 #define BuildFilename2(file1, file2) g_build_filename(getInstallDir(), file1, file2, NULL)
 #else
@@ -34,6 +35,7 @@
 
 #ifdef WIN32
 extern char *getInstallDir( void );
+extern char *getDocDir( void );
 #endif
 
 extern void PrintSystemError(const char* message);
