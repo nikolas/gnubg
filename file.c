a405dcae18b155d35c1b0334849319128622ec8a
Jon Kinsey <jonkinsey@gmail.com>
2009-06-29 19:48:04 +0000

    Include needed for free()

--- file.c
+++ file.c
@@ -15,13 +15,14 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: file.c,v 1.19 2009/03/01 20:01:49 Superfly_Jon Exp $
+ * $Id: file.c,v 1.20 2009/06/29 19:17:03 Superfly_Jon Exp $
  */
 
 #include "config.h"
 #include "backgammon.h"
 #include <glib/gstdio.h>
 #include "file.h"
+#include <stdlib.h>
 
 ExportFormat export_format[] = {
 	{EXPORT_SGF, ".sgf", N_("Gnu Backgammon File"), "sgf", {TRUE, TRUE, TRUE}},	/*must be the first element */
