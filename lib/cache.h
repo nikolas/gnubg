53f867b551f665833f8a22dd9b4f3f1cb6b2c48b
Philippe Michel <philippe.michel7@free.fr>
2011-07-14 21:11:03 +0000

    Use gcc builtins for locking on Intel CPUs

--- lib/cache.h
+++ lib/cache.h
@@ -15,7 +15,7 @@
  * cache.h
  *
  * by Gary Wong, 1997-2000
- * $Id: cache.h,v 1.17 2009/08/30 20:56:32 Superfly_Jon Exp $
+ * $Id: cache.h,v 1.18 2011/05/08 19:08:50 plm Exp $
  */
 
 #ifndef _CACHE_H_
@@ -40,7 +40,7 @@ typedef struct _cacheNode
 	cacheNodeDetail nd_primary;
 	cacheNodeDetail nd_secondary;
 #if USE_MULTITHREAD
-	int lock;
+	volatile int lock;
 #endif
 } cacheNode;
 
