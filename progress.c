d62d3a6027d0599fa3744d8c920133235e398c9b
Michael Petch <mpetch@gnubg.org>
2011-09-03 02:28:34 +0000

    Fix code with potentially undefined results per C standard

--- progress.c
+++ progress.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: progress.c,v 1.62 2010/11/11 16:50:21 plm Exp $
+ * $Id: progress.c,v 1.63 2011/02/08 18:57:56 c_anthon Exp $
  */
 
 #include "config.h"
@@ -1084,7 +1084,8 @@ static void TextRolloutProgress(float aarOutput[][NUM_ROLLOUT_OUTPUTS],
 
 	}
 
-	prp->iNextAlternative = (++prp->iNextAlternative) % prp->n;
+	prp->iNextAlternative++;
+	prp->iNextAlternative = (prp->iNextAlternative) % prp->n;
 	if (iAlternative == (prp->n - 1))
 		prp->iNextGame += prc->nTrials / 10;
 
