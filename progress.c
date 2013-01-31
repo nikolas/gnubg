c48613c3f65932cccce0f6e974c6beff0151fb46
Philippe Michel <philippe.michel7@free.fr>
2013-01-31 22:15:49 +0000

    Fix type mismatch in memory allocation.

--- progress.c
+++ progress.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: progress.c,v 1.65 2012/09/01 18:49:57 plm Exp $
+ * $Id: progress.c,v 1.66 2012/11/03 20:49:26 plm Exp $
  */
 
 #include "config.h"
@@ -84,7 +84,7 @@ static void AllocTextList(rolloutprogress *prp)
 {	/* 2d array to cache displayed widget text */
 	int i;
 	int lines = prp->n;
-	prp->pListText = malloc(sizeof(char*) * lines * 2);
+	prp->pListText = malloc(sizeof(char**) * lines * 2);
 
 	for (i = 0; i < lines; i++)
 	{
