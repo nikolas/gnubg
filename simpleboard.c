d5e847376f58d6b072e93047461002539fc911f5
Philippe Michel <philippe.michel7@free.fr>
2013-05-30 06:14:13 +0000

    Clarify code for cube drawing in cairo graphics

--- simpleboard.c
+++ simpleboard.c
@@ -11,7 +11,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: simpleboard.c,v 1.19 2013/01/23 21:12:23 plm Exp $
+ * $Id: simpleboard.c,v 1.20 2013/01/23 22:12:34 plm Exp $
  */
 
 /*! \file simpleboard.c
@@ -202,7 +202,7 @@ static void draw_points(SimpleBoard * board)
 
 static void draw_cube(SimpleBoard * board)
 {
-	gint x, y;
+	gint x = 150, y = 150;
 	gchar *text;
 	SimpleBoardColor color = board->color_cube;
 	cairo_t *cr = board->cr;
@@ -214,29 +214,26 @@ static void draw_cube(SimpleBoard * board)
 			x = 200;
 		else
 			x = 100;
-		y = 142;
 		cube *= 2;
 	} else {
-		x = 150;
 		switch (board->ms.fCubeOwner) {
 		case -1:
-			y = 142;
 			break;
 		case 0:
-			y = 32;
+			y = 40;
 			break;
 		case 1:
-			y = 252;
+			y = 260;
 			break;
 		default:
 			g_assert_not_reached();
 		}
 	}
 
-	cairo_rectangle(cr, x - 8, y, 16, 16);
+	cairo_rectangle(cr, x - 8, y - 8, 16, 16);
 	fill_and_stroke(cr, color);
 	text = g_strdup_printf("%d", cube);
-	cairo_move_to(cr, x, y + 8);
+	cairo_move_to(cr, x, y);
 	draw_centered_text(cr, color.text, 10.0-2*floorf(log10f(cube)), text);
 	g_free(text);
 }
