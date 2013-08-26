dc002086a4af442e1255b266eedef5daaa2f9e36
Philippe Michel <philippe.michel7@free.fr>
2013-08-26 20:36:09 +0000

    Remove reference to obsolete URL

--- import.c
+++ import.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: import.c,v 1.178 2013/07/03 19:09:26 plm Exp $
+ * $Id: import.c,v 1.179 2013/07/03 19:17:42 plm Exp $
  */
 
 #include "config.h"
@@ -2726,7 +2726,7 @@ ImportTMG(FILE * pf, const char *UNUSED(szFilename))
 
     for (j = 0; j < 2; ++j)
         SetMatchInfo(&mi.pchRating[j], NULL, NULL);
-    SetMatchInfo(&mi.pchPlace, "TrueMoneyGames (http://www.truemoneygames.com)", NULL);
+    SetMatchInfo(&mi.pchPlace, "TrueMoneyGames", NULL);
     SetMatchInfo(&mi.pchEvent, NULL, NULL);
     SetMatchInfo(&mi.pchRound, NULL, NULL);
     SetMatchInfo(&mi.pchAnnotator, NULL, NULL);
