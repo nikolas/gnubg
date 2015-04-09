3ab15d3d627ee35b0a124e2fbe730b1c3231a14b
Philippe Michel <philippe.michel7@free.fr>
2015-04-09 21:27:32 +0000

    Silence warnings from cppcheck

--- export.c
+++ export.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: export.c,v 1.79 2014/01/24 22:53:28 plm Exp $
+ * $Id: export.c,v 1.80 2014/10/19 21:24:54 plm Exp $
  */
 
 #include "config.h"
@@ -163,8 +163,7 @@ draw_simple_board_on_cairo(matchstate * sb_pms,
 
     simple_board_draw(board);
     g_free(board);
-    if (header)
-        g_string_free(header, TRUE);
+    g_string_free(header, TRUE);
     if (annotation)
         g_string_free(annotation, TRUE);
     return 1;
@@ -1168,7 +1167,7 @@ ExportGameJF(FILE * pf, listOLD * plGame, int iGame, int withScore, int fSst)
 
         if ((n = GameStatus((ConstTanBoard) anBoard, ms.bgv))) {
             fprintf(pf, "%sWins %d point%s%s\n\n",
-                    i & 1 ? "                                  " : "\n      ",
+                    (i & 1) ? "                                  " : "\n      ",
                     n * nFileCube, n * nFileCube > 1 ? "s" : "", "" /* FIXME " and the match" if appropriate */ );
         }
         i++;
