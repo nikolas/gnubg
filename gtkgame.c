21334a6e10f8c48648962d5cdecd92efb09c4685
Philippe Michel <philippe.michel7@free.fr>
2016-01-11 22:43:13 +0000

    Add an "Add to database" button to the Match or session statistics panel

--- gtkgame.c
+++ gtkgame.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: gtkgame.c,v 1.896 2015/10/14 22:19:23 plm Exp $
+ * $Id: gtkgame.c,v 1.897 2016/01/11 21:36:29 plm Exp $
  */
 
 #include "config.h"
@@ -6788,6 +6788,7 @@ GTKDumpStatcontext(int game)
 {
     GtkWidget *copyMenu, *menu_item, *pvbox, *pwUsePanels;
     GtkWidget *navi_combo;
+    GtkWidget *addToDbButton;
 #if defined(USE_BOARD3D)
     int i;
     GtkWidget *pw;
@@ -6795,6 +6796,10 @@ GTKDumpStatcontext(int game)
     GraphData *gd = CreateGraphData();
 #endif
     pwStatDialog = GTKCreateDialog("", DT_INFO, NULL, DIALOG_FLAG_MODAL, NULL, NULL);
+
+    gtk_container_add(GTK_CONTAINER(DialogArea(pwStatDialog, DA_BUTTONS)),
+                      addToDbButton = gtk_button_new_with_label(_("Add to DB")));
+    g_signal_connect(addToDbButton, "clicked", G_CALLBACK(GtkRelationalAddMatch), pwStatDialog);
 
     pwNotebook = gtk_notebook_new();
     gtk_notebook_set_scrollable(GTK_NOTEBOOK(pwNotebook), TRUE);
