fb1a55418ad2e8a03bffaf8b3cd6e025a590e495
Philippe Michel <philippe.michel7@free.fr>
2016-06-05 20:02:34 +0000

    Remove superfluous #include

--- lib/neuralnet.c
+++ lib/neuralnet.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: neuralnet.c,v 1.85 2014/11/23 20:17:48 plm Exp $
+ * $Id: neuralnet.c,v 1.86 2016/06/05 19:46:20 plm Exp $
  */
 
 #include "config.h"
@@ -24,7 +24,6 @@
 #include "common.h"
 #include <glib.h>
 #include <errno.h>
-#include <isaac.h>
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
