e29526f87fdb1b71b6cfb2c221adeee22ccdef00
Philippe Michel <philippe.michel7@free.fr>
2016-06-07 20:31:54 +0000

    Fix unreachable return

--- lib/neuralnet.c
+++ lib/neuralnet.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: neuralnet.c,v 1.86 2016/06/05 19:46:20 plm Exp $
+ * $Id: neuralnet.c,v 1.87 2016/06/05 20:01:42 plm Exp $
  */
 
 #include "config.h"
@@ -500,7 +500,6 @@ CheckSSE(void)
 #endif
     if (0 != error)
         result = 0;
-    return result;
 
 #else
 
