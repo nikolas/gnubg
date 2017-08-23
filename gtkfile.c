542596f220337e3d58c3c49b776638b798191373
Philippe Michel <philippe.michel7@free.fr>
2017-08-23 21:13:59 +0000

    Get out of edit mode before loading or importing a match

--- gtkfile.c
+++ gtkfile.c
@@ -19,7 +19,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: gtkfile.c,v 1.68 2017/08/23 20:48:40 plm Exp $
+ * $Id: gtkfile.c,v 1.69 2017/08/23 20:58:21 plm Exp $
  */
 
 #include "config.h"
@@ -37,6 +37,7 @@
 
 #include "gtkfile.h"
 #include "gtkgame.h"
+#include "gtktoolbar.h"
 #include "gtkwindows.h"
 #include "file.h"
 #include "util.h"
@@ -334,9 +335,10 @@ do_import_file(gint import_type, gchar * fn)
         cmd = g_strdup_printf("import %s \"%s\"", import_format[import_type].clname, fn);
     }
     if (cmd)
+        if (ToolbarIsEditing(NULL))
+            click_edit();
         UserCommand(cmd);
     g_free(cmd);
-
 }
 
 extern void
