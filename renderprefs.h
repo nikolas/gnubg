6038fafc11ac91987ea78af19ffc74bd81000e8f
Philippe Michel <philippe.michel7@free.fr>
2019-04-26 23:09:38 +0000

    Silence compiler warning

--- renderprefs.h
+++ renderprefs.h
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: renderprefs.h,v 1.10 2012/08/27 23:06:40 plm Exp $
+ * $Id: renderprefs.h,v 1.11 2013/06/16 02:16:20 mdpetch Exp $
  */
 
 #ifndef RENDERPREFS_H
@@ -33,7 +33,7 @@ extern void CopyAppearance(renderdata * prd);
 extern void RenderPreferencesParam(renderdata * prd, const char *szParam, char *szValue);
 extern void SaveRenderingSettings(FILE * pf);
 
-#if USE_BOARD3D
+#if defined(USE_BOARD3D)
 char *WriteMaterial(Material * pMat);
 char *WriteMaterialDice(renderdata * prd, int num);
 #endif
