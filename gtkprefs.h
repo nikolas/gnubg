dcd67af1a27c954e5b2394f8d7a6ba7a00355efe
Philippe Michel <philippe.michel7@free.fr>
2019-11-03 15:26:13 +0000

    Add proper copyright attributions as suggested by FSF.

--- gtkprefs.h
+++ gtkprefs.h
@@ -1,11 +1,11 @@
 /*
- * gtkprefs.h
+ * Copyright (C) 2001-2003 Gary Wong <gtw@gnu.org>
+ * Copyright (C) 2002-2015 the AUTHORS
  *
- * by Gary Wong <gtw@gnu.org>, 2000.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of version 3 or later of the GNU General Public License as
- * published by the Free Software Foundation.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,10 +13,9 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * $Id: gtkprefs.h,v 1.16 2013/06/16 02:16:16 mdpetch Exp $
+ * $Id: gtkprefs.h,v 1.17 2015/01/01 16:42:05 plm Exp $
  */
 
 #ifndef GTKPREFS_H
