f51c39436c3064d5209b633059d38eba0619f6e1
Philippe Michel <philippe.michel7@free.fr>
2019-11-14 20:41:05 +0000

    Add proper copyright attributions as suggested by FSF.

--- board3d/model.c
+++ board3d/model.c
@@ -1,13 +1,10 @@
 /*
- * model.c
- * by Jon Kinsey, 2003
+ * Copyright (C) 2003-2019 Jon Kinsey <jonkinsey@gmail.com>
  *
- * Shadow model representation
- *
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of version 3 or later of the GNU General Public License as
- * published by the Free Software Foundation.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,10 +12,9 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * $Id: model.c,v 1.24 2019/11/09 22:23:55 Superfly_Jon Exp $
+ * $Id: model.c,v 1.25 2019/11/13 21:41:05 Superfly_Jon Exp $
  */
 
 #include "config.h"
