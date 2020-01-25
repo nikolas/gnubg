3221ca8ec7e8f72fce6f612be0ab9ef54fbcd4fc
Philippe Michel <philippe.michel7@free.fr>
2020-01-25 21:38:02 +0000

    Add proper copyright attributions as suggested by FSF.

--- gtkchequer.h
+++ gtkchequer.h
@@ -1,11 +1,11 @@
 /*
- * gtkchequer.h
+ * Copyright (C) 2002-2003 Joern Thyssen <jthyssen@dk.ibm.com>
+ * Copyright (C) 2004-2009 the AUTHORS
  *
- * by Joern Thyssen <jthyssen@dk.ibm.com>, 2002
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of version 3 or later of the GNU General Public License as
- * published by the Free Software Foundation.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,10 +13,9 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * $Id: gtkchequer.h,v 1.20 2012/08/27 23:06:42 plm Exp $
+ * $Id: gtkchequer.h,v 1.21 2013/06/16 02:16:14 mdpetch Exp $
  */
 
 #ifndef GTKCHEQUER_H
