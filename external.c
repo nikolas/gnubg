a14f3ce498d689af1565c86ebce49750e038bc61
Philippe Michel <philippe.michel7@free.fr>
2020-04-04 19:54:08 +0000

    Silence clang static analyzer warnings (dead nested assignments)

--- external.c
+++ external.c
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * $Id: external.c,v 1.101 2019/11/30 21:55:13 plm Exp $
+ * $Id: external.c,v 1.102 2020/02/23 20:54:56 plm Exp $
  */
 
 #include "config.h"
@@ -397,7 +397,7 @@ ExtEvaluation(scancontext * pec)
     evalcontext ec;
 
     if (ProcessFIBSBoardInfo(&pec->bi, &processedBoard)) {
-        return szResponse = g_strdup_printf("Error: badly formed board\n");
+        return g_strdup_printf("Error: badly formed board\n");
     }
 
     anScore[0] = processedBoard.nScore;
@@ -447,7 +447,7 @@ ExtFIBSBoard(scancontext * pec)
     char *szResponse;
 
     if (ProcessFIBSBoardInfo(&pec->bi, &processedBoard))
-        return szResponse = g_strdup_printf("Error: badly formed board\n");
+        return g_strdup_printf("Error: badly formed board\n");
 
     anScore[0] = processedBoard.nScore;
     anScore[1] = processedBoard.nScoreOpp;
