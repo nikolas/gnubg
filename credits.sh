dfd1fb4c2f8fd80212c4664d9419f9c2e2d4faab
Philippe Michel <philippe.michel7@free.fr>
2021-04-03 21:06:12 +0000

    New year

--- credits.sh
+++ credits.sh
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #
-# $Id: credits.sh,v 1.160 2019/10/13 13:45:10 plm Exp $
+# $Id: credits.sh,v 1.161 2020/01/11 22:19:06 plm Exp $
 # 
 
 authors=/tmp/auth.$$
@@ -268,7 +268,7 @@ cat > credits.c <<EOF
 #include "credits.h"
 
 const char aszCOPYRIGHT[] = N_("Copyright (C) 1999-2004 Gary Wong.\n"
-                               "Copyright (C) 2004-2020 the AUTHORS; for details type \`show version'.");
+                               "Copyright (C) 2004-2021 the AUTHORS; for details type \`show version'.");
 
 credEntry ceAuthors[] = {
 EOF
