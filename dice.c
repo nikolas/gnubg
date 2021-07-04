58139af82b04273f89250d8d0d5a824501439f16
Philippe Michel <philippe.michel7@free.fr>
2021-07-04 22:39:38 +0000

    Document quirk in Mersenne RNG initialization. Silence compiler warnings.

--- dice.c
+++ dice.c
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * $Id: dice.c,v 1.104 2021/03/07 14:03:21 plm Exp $
+ * $Id: dice.c,v 1.105 2021/07/04 22:17:53 plm Exp $
  */
 
 #include "config.h"
@@ -520,7 +520,8 @@ InitRNGSeedMP(mpz_t n, rng rng, rngcontext * rngctx)
 
                 achState = mpz_export(NULL, &cb, -1, sizeof(gint32), 0, 0, n);
                 for (i = 0; i < MT_ARRAY_N && i < cb; i++) {
-                    tempmtkey[i] = achState[i];
+                    /* FIXME?  Initializing an unsigned long with a guint32 */
+                    tempmtkey[i] = (unsigned long) achState[i];
                 }
                 for (; i < MT_ARRAY_N; i++) {
                     tempmtkey[i] = 0;
@@ -610,6 +611,7 @@ CloseRNG(const rng rngx, rngcontext * rngctx)
     case RNG_FILE:
         /* close file */
         CloseDiceFile(rngctx);
+        break;
 
     default:
         /* no-op */
@@ -834,6 +836,8 @@ RollDice(unsigned int anDice[2], rng * prng, rngcontext * rngctx)
         anDice[0] = ReadDiceFile(rngctx);
         anDice[1] = ReadDiceFile(rngctx);
         rngctx->c += 2;
+
+        break;
 
     default:
         break;
