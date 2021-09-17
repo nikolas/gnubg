ba72139a5f174531468a8b60be1cbb9ec9e9358d
Philippe Michel <philippe.michel7@free.fr>
2021-09-17 21:30:03 +0000

    Add credit for Spanish translation

--- credits.sh
+++ credits.sh
@@ -17,7 +17,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #
-# $Id: credits.sh,v 1.161 2020/01/11 22:19:06 plm Exp $
+# $Id: credits.sh,v 1.162 2021/04/03 21:05:18 plm Exp $
 # 
 
 authors=/tmp/auth.$$
@@ -71,6 +71,7 @@ Renzo Campagna,Italian
 Yoshito Takeuchi,Japanese
 Mihai Varzaru,Romanian
 Dmitri I Gouliaev,Russian
+Fernando García García,Spanish
 Akif Dinc,Turkish
 EOF
 
@@ -222,12 +223,12 @@ cat > credits.h <<EOF
 
 #include <glib/gi18n.h>
 
-typedef struct _credEntry {
+typedef struct {
 	char* Name;
 	char* Type;
 } credEntry;
 
-typedef struct _credits {
+typedef struct {
 	const char* Title;
 	credEntry *Entry;
 } credits;
