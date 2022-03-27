6544ae68c929605a972ba51e30f6cf4ebbd82779
Philippe Michel <philippe.michel7@free.fr>
2022-03-27 18:06:52 +0000

    Fix memory leak

--- output.c
+++ output.c
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * $Id: output.c,v 1.6 2019/11/21 22:05:37 plm Exp $
+ * $Id: output.c,v 1.7 2022/01/27 22:14:47 plm Exp $
  */
 
 #include "config.h"
@@ -235,12 +235,12 @@ print_utf8_to_locale(const gchar *sz)
     GError *error = NULL;
     gchar *szl = g_locale_from_utf8(sz, -1, NULL, NULL, &error);
 
-#if 0
     if (error) {
+#if 0
         g_printerr("g_locale_from_utf8 failed: %s\n", error->message);
+#endif
         g_error_free(error);
     }
-#endif
 
     if (szl != NULL)
         printf("%s", szl);
