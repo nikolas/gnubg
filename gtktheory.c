8318b0a0ba88760607b02f6e931a8c41121a661e
Philippe Michel <philippe.michel7@free.fr>
2022-09-02 13:44:24 +0000

    Fix gtk_paint_layout related GTK3 deprecation warnings

--- gtktheory.c
+++ gtktheory.c
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
- * $Id: gtktheory.c,v 1.76 2022/08/29 21:01:06 plm Exp $
+ * $Id: gtktheory.c,v 1.77 2022/08/30 18:38:35 plm Exp $
  */
 
 #include "config.h"
@@ -506,9 +506,14 @@ GraphDraw(GtkWidget * pwGraph, cairo_t * cr, theorywidget * ptw)
             g_free(sz);
 
             pango_layout_get_pixel_size(layout, &width, &height);
-            gtk_locdef_paint_layout(gtk_widget_get_style(pwGraph), gtk_widget_get_window(pwGraph), cr,
-                                    GTK_STATE_NORMAL, TRUE, NULL, pwGraph, "label",
-                                    x + cx * i / 20 - width / 2 /* FIXME */ , y - height - 1, layout);
+#if GTK_CHECK_VERSION(3,0,0)
+            gtk_render_layout(gtk_widget_get_style_context(pwGraph), cr,
+                                (double)(x + cx * i / 20 - width / 2) /* FIXME */, (double)(y - height - 1), layout);
+#else
+            gtk_paint_layout(gtk_widget_get_style(pwGraph), gtk_widget_get_window(pwGraph),
+                                GTK_STATE_NORMAL, TRUE, NULL, pwGraph, "label",
+                                x + cx * i / 20 - width / 2 /* FIXME */ , y - height - 1, layout);
+#endif
         }
     }
     g_object_unref(layout);
